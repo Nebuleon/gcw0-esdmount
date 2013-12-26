f05dcb7fc4e4be4984168d91079fe13ab6ee57a2
Nebuleon Fumika <nebuleon@porygon>
2013-12-26 02:42:22 +0000

    Remove the mounter, and try to augment the unmounter (but fail).

--- umount.sh
+++ umount.sh
@@ -1,39 +1,71 @@
 #!/bin/sh
 
 MOUNTLIST=/proc/mounts
-DEVICE=/dev/mmcblk1p1
 
 export DIALOGOPTS="--colors --backtitle \"Unmount external microSD\""
 echo "screen_color = (RED,RED,ON)" > /tmp/dialog_err.rc
 
-existing_mount_line=$(grep "$DEVICE" "$MOUNTLIST")
+# Assemble the mount points that can be unmounted.
 
-if [ "x$existing_mount_line" == x ]
+mmcblk_devices="/dev/mmcblk[1-9]*"
+if [ "$mmcblk_devices" = '/dev/mmcblk[1-9]*' ]
 then
-  dialog --msgbox "Your external microSD card was already unmounted and can now be ejected safely." 0 0
+  mmcblk_devices=
+fi
+
+usb_devices="/dev/sd[a-z]*"
+if [ "$usb_devices" = '/dev/sd[a-z]*' ]
+then
+  usb_devices=
+fi
+
+devices="$mmcblk_devices $usb_devices"
+echo "$devices"
+if [ -z "$devices" ]
+then
+  dialog --msgbox "You have no storage devices to unmount." 0 0
   exit 0
 fi
 
-umount_output=$(umount "$DEVICE" 2>&1)
-umount_result=$?
+# Here at least one can be unmounted. Prepare the argument list for the unmount dialog.
+args=
+for device in $devices
+do
+  args="$args "$device" "$device" ''"
+done
 
-if [ $umount_result -ne 0 ]
+device_to_umount=$(exec 3>&1; dialog --output-fd 3 --radiolist "Select the device to unmount." 0 0 0 $args 3>&1 >&2; 3>&-)
+
+if [ -z "$device_to_umount" ]
 then
-  # It's possible that umount says it failed, but it really didn't.
-  # Double-check before reporting this as a failure.
-  if ! grep "$DEVICE" "$MOUNTLIST"
+  exit 0  # Cancelled
+else
+  umount_output=$(umount "$device_to_umount" 2>&1)
+  umount_result=$?
+
+  if [ $umount_result -ne 0 ]
   then
-    dialog --msgbox "You can now safely eject your external microSD card." 0 0
-    exit 0
-  else
-    DIALOGRC=/tmp/dialog_err.rc dialog --msgbox "Your external microSD card was not unmounted and cannot be ejected safely.
+    # It's possible that umount says it failed, but it really didn't.
+    # Double-check before reporting this as a failure.
+    if ! grep "$device_to_umount" "$MOUNTLIST"
+    then
+      dialog --msgbox "You can now safely remove the following device:
+
+$device_to_umount" 0 0
+      exit 0
+    else
+      DIALOGRC=/tmp/dialog_err.rc dialog --msgbox "The device was not unmounted and cannot be ejected safely.
 
 umount exited with status code $umount_result
 
 $umount_output" 0 0
-    exit 1
+      exit 1
+    fi
+  else
+    dialog --msgbox "You can now safely remove the following device:
+
+$device_to_umount" 0 0
+    exit 0
   fi
-else
-  dialog --msgbox "You can now safely eject your external microSD card." 0 0
-  exit 0
 fi
+
